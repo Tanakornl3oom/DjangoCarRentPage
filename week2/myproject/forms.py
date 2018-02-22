9bca817faaee0c11d0f1bd56bb331e06d5260ade
Tanakorn Peetivorapat <l3oom@Tanakorns-MacBook-Pro.local>
2018-02-23 01:33:48 +0700

    first push

---
+++ week2/myproject/forms.py
@@ -0,0 +1,60 @@
+from django import forms
+from django.forms import ModelForm
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+# from django.forms.extras.widgets import SelectDateWidget
+from django.contrib.admin import widgets
+
+from .models import Person,Rent,Car,User
+
+class PersonForm(ModelForm):
+	class Meta:
+		model =  Person
+		exclude=[]
+
+		# widgets = {
+		# 	'dob': forms.DateInput(
+		# 		attrs={
+		# 		'type': 'date', 
+		# 		'value': datetime.datetime.now().strftime("%Y-%m-%d")
+		# 		}, format="%Y-%m-%d"
+		# 	),
+		# }
+		
+	def __init__(self, *args, **kwargs):
+		super(PersonForm, self).__init__(*args, **kwargs)
+		self.helper = FormHelper()
+		self.helper.add_input(Submit('submit', 'Submit'))
+
+class RentForm(ModelForm):
+	# user = forms.ModelChoiceField(queryset=User.objects.all())
+	car = forms.ModelChoiceField(queryset=Car.objects.all(),required = True)
+	start_datetime = forms.DateTimeField(help_text='Required. Format: YYYY-MM-DD HH:MM:SS',required = True)
+	stop_datetime = forms.DateTimeField(help_text='Required. Format: YYYY-MM-DD HH:MM:SS',required = True)
+	fee = forms.DecimalField(help_text='Required. Format: 0.00',required = True)
+
+
+	class Meta:
+		model =  Rent 
+		fields = ['car', 'start_datetime', 'stop_datetime', 'fee' ]
+		exclude=['user']
+		# field1 = forms.ModelChoiceField(queryset=Car, empty_label="(Nothing)")
+
+
+		# widgets = {
+		# 	'dob': forms.DateInput(
+		# 		attrs={
+		# 		'type': 'date', 
+		# 		'value': datetime.datetime.now().strftime("%Y-%m-%d")
+		# 		}, format="%Y-%m-%d"
+		# 	),
+		# }
+		
+	def __init__(self, *args, **kwargs):
+		super(RentForm, self).__init__(*args, **kwargs)
+		self.helper = FormHelper()
+		self.helper.help_text_inline = True
+		self.helper.add_input(Submit('submit', 'Submit'))
+
+
+
---
+++ week2/myproject/views.py
@@ -0,0 +1,57 @@
+from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
+from .forms import PersonForm , RentForm
+from .models import Person , User , Rent , Car
+
+
+from django.shortcuts import render
+# Create your views here.
+
+# class CreateProfileView(CreateView):
+# 	queryset = User()
+# 	template_name='user.html'
+# 	form_class = UserForm
+# 	success_url = '/admin'
+
+# class CreatePersonView(CreateView):
+# 	queryset = Person()
+# 	template_name='create.html'
+# 	form_class = PersonForm
+# 	success_url = '/admin'
+
+class CreateRentView(CreateView):
+	queryset = Rent() ,Car()
+	template_name='rent.html'
+	form_class = RentForm
+	success_url = '/profiles/'
+
+	def form_valid(self,form):
+		form.instance.user = self.request.user
+		return super(CreateRentView, self).form_valid(form)
+ 
+
+# class ListPersonView(ListView):
+# 		model = Person
+# 		template_name='list.html'
+
+class ListCarView(ListView):
+		model = Car
+		template_name='Car.html'
+
+
+
+
+# def login(request):
+#     if request.method == 'POST':
+#         print('dfsasfas')
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
+#         user = authenticate(username=username, password=password)
+#         if  login(request, user) :
+#             request.session['username'] = username
+#             return redirect('/admin')
+#         else:
+#             messages.error(request, 'Error wrong username/password')
+#     return render(request, 'login.html')
+		
+				
